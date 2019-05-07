ce660cc701a9f4f6377d84ffbabfb83692dcb321
Christopher Anderson <canderson30@unl.edu>
2019-05-07 12:31:53 -0400

    Update conda_install_linux.sh

--- conda_install_linux.sh
+++ conda_install_linux.sh
@@ -9,10 +9,4 @@ $curl_or_wget https://raw.githubusercontent.com/chrisLanderson/MetaOmics_Worksho
 bash Miniconda3-latest-Linux-x86_64.sh -b -f -p miniconda3
 miniconda3/bin/conda env create -n omics-env -f omics_env_linux.yml
 miniconda3/bin/conda env create -n qiime2-env -f qiime2_env_linux.yml
-source miniconda3/bin/activate omics-env
-wget –q https://github.com/mothur/mothur/releases/download/v1.42.0/Mothur.linux_64.zip
-unzip -oqq Mothur.linux_64.zip
-mv mothur/* miniconda3/envs/omics-env/bin/
-rm -r mothur
 rm Miniconda3-latest-MacOSX-x86_64.sh
-rm Mothur.mac_64.OSX-10.13.zip
